293e9bb419f3d9819571a4c263c12653a95e7f48
BGDTC <81570834+bgdtc@users.noreply.github.com>
2023-04-15 18:36:47 +0200

    refactor(linting-refactor): refactored+linted (#12) (#17)

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,10 @@
 import serverless from "serverless-http";
 import express from "express";
 import router from "./router";
-
+import bodyParser from 'body-parser';
 const app = express();
 
-app.use(express.json());
-
+app.use(bodyParser.urlencoded());
 app.use("/api", router);
 
 module.exports.handler = serverless(app);
