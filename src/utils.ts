22bfcc24330ed68a6e186bce2a894c468151b599
BGDTC <81570834+bgdtc@users.noreply.github.com>
2023-04-15 14:13:54 +0200

    refactor(linting-refactor): refactored+linted (#12)

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,5 @@
 export const mailRgx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const tableName = process.env.DYNAMODB_TABLE_NAME! || 'TokensRateLimit';
+export const rateLimit = Number(process.env.JUSTIFY_WORD_LIMIT! || 80000);
+export const now = new Date();
+export const hPlus24 = now.setHours(now.getHours() + 24);
